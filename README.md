7bdede41ab3a709483d2a7138ffe4dd26e9d7be7
sagar <sagarhani33@gmail.com>
2015-08-10 22:22:36 +0530

    moving into gh-pages

---
+++ README.md
@@ -0,0 +1,2 @@
+# snake-and-apple
+A HTML5 game using phaser framework.
---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+var game;
+
+game = new Phaser.Game(1366, 673, Phaser.AUTO, '');
+
+game.state.add('Menu', Menu);
+game.state.add('Game', Game);
+game.state.add('Game_Over', Game_Over);
+
+game.state.start('Menu');
